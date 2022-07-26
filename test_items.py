7ea3b1d93e0a275e7e5332c07ee213bf9cadbfee
babyavo <109292074+babyavo@users.noreply.github.com>
2022-07-26 18:20:15 +0800

    Add files via upload

---
+++ test_items.py
@@ -0,0 +1,11 @@
+from selenium.webdriver.common.by import By
+
+
+class TestMainPage1():
+    def test_check_basket_button(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+        browser.get(link)
+        #button = browser.find_element(By.CSS_SELECTOR, "type = 'submit'")
+        assert browser.find_elements(By.CSS_SELECTOR,"[type = 'submit']"), 'basket button not found'
+
+
