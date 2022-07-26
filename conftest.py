63f1a5034ce35ac5742d20fd0d1937518596a4a3
babyavo <109292074+babyavo@users.noreply.github.com>
2022-07-26 14:12:30 +0300

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -4,7 +4,7 @@ from selenium import webdriver
 
 def pytest_addoption(parser):
     parser.addoption('--language', action='store', default=None,
-                     help="Choose language: en or ru")
+                     help="Choose language")
 
 
 @pytest.fixture(scope="function")
