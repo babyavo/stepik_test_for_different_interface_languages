e254ccd3d614c3592052dbcedffb81ae7ce43235
babyavo <anhelinasiklichuk@gmail.com>
2022-08-30 11:31:38 +0300

    mplement 123

--- test_items.py
+++ test_items.py
@@ -5,7 +5,4 @@ class TestMainPage1():
     def test_check_basket_button(self, browser):
         link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
         browser.get(link)
-        #button = browser.find_element(By.CSS_SELECTOR, "type = 'submit'")
         assert browser.find_elements(By.CSS_SELECTOR,"[type = 'submit']"), 'basket button not found'
-
-
